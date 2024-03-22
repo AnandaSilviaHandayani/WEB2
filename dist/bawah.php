d0160f3685a442f479654ae497243b215d23347a
AnandaSilviaHandayani <anan23172si@student.nurulfikri.ac.id>
2024-03-22 15:15:50 +0700

    memasukan data baru

---
+++ dist/bawah.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Ananda Silvia</a>
+        </div>
+        <div class="footer-right">
+          
+        </div>
+      </footer>
+    </div>
+  </div>
+<!-- General JS Scripts -->
+      
+ 
+</body>
+</html>
---
+++ dist/index.php
@@ -0,0 +1,24 @@
+<?php
+include 'atas.php';
+?>
+
+      <!-- Main Content -->
+      <div class="main-content">
+<br><br>
+          <div class="row">
+          <h4>Apa Itu PHP?</h4>    
+            <p>PHP Adalah salah satu bahasa pemrograman yang wajib dipelajari. Alasannya, bahasa pemrograman ini mampu untuk membuat website menjadi dinamis. </p>
+                
+          <h4>Fungsi PHP :</h4>
+             <p> Secara umum, fungsi PHP adalah digunakan untuk pengembangan website. Baik website statis seperti situs berita yang tidak membutuhkan banyak fitur. Ataupun website dinamis seperti toko online dengan segudang fitur pendukung.</p>
+               
+             
+             <div class="card-body">
+                  <canvas id="myChart" height="1000" width="500"></canvas>
+                </div>
+              </div>
+            </div>
+           
+<?php
+include 'bawah.php';
+?>
